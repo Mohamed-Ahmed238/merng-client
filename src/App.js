a742a85bd318eafe1ac60d89fca0a6fc53a1caad
Mohamed Khalifa <mokhalifa238@gmail.com>
2022-03-24 15:11:49 +0200

    Adding footer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { Container } from 'semantic-ui-react'
 import MenuBar from './Component/MenuBar'
 import Home from './Pages/Home'
 import Register from './Pages/Register'
+import Footer from './Component/Footer'
 import Login from './Pages/Login'
 import SinglePost from './Pages/SinglePost'
 import { useContext } from 'react'
@@ -18,25 +19,28 @@ import { AuthContext } from './Context/auth'
 function App() {
   const { user } = useContext(AuthContext)
   return (
-    <Container>
-      <Router>
-        <MenuBar />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route
-            exact
-            path='/login'
-            element={user ? <Navigate replace to='/' /> : <Login />}
-          />
-          <Route
-            exact
-            path='/register'
-            element={user ? <Navigate replace to='/' /> : <Register />}
-          />
-          <Route exact path='/posts/:postId' element={<SinglePost />} />
-        </Routes>
-      </Router>
-    </Container>
+    <>
+      <Container>
+        <Router>
+          <MenuBar />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route
+              exact
+              path='/login'
+              element={user ? <Navigate replace to='/' /> : <Login />}
+            />
+            <Route
+              exact
+              path='/register'
+              element={user ? <Navigate replace to='/' /> : <Register />}
+            />
+            <Route exact path='/posts/:postId' element={<SinglePost />} />
+          </Routes>
+        </Router>
+      </Container>
+      <Footer />
+    </>
   )
 }
 
